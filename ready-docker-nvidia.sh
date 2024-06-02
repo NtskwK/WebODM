706109559c03033a49834ae8bc91e290bec35533
Ntkskwk <natsukawa247@outlook.com>
2024-06-02 13:10:03 +0800

    feat: docker-compose prepare for nvdia GPU

---
+++ ready-docker-nvidia.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Ntkskwk@github
+# 2024/04/07
+
+# 检查当前用户是否有sudo权限
+if [ "$(id -u)" != "0" ]; then
+    echo "错误：请以sudo权限运行此脚本！"
+    exit 1
+fi
+
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt-get update
+
+apt-get install -y nvidia-container-toolkit
+
+docker run --rm --runtime=nvidia --gpus all ubuntu nvidia-smi
+
+# https://docs.docker.com/engine/install/ubuntu/
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run hello-world
+
+apt-get install docker-compose-plugin
+
+docker compose version
