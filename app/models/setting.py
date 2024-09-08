d251e09b3f2b37a18cf4c95086f033524dab7331
Ntkskwk <natsukawa247@outlook.com>
2024-09-08 21:09:52 +0800

    chore: 修改应用信息

--- app/models/setting.py
+++ app/models/setting.py
@@ -17,7 +17,7 @@ logger = logging.getLogger('app.logger')
 
 
 class Setting(models.Model):
-    app_name = models.CharField(max_length=255, blank=False, null=False, help_text=_("The name of your application"), verbose_name=_("App name"))
+    app_name = models.CharField(default='橙黄橘绿', max_length=255, blank=False, null=False, help_text=_("The name of your application"), verbose_name=_("App name"))
     app_logo = models.ImageField(upload_to="settings/", blank=False, null=False, help_text=_("A 512x512 logo of your application (.png or .jpeg)"), verbose_name=_("App logo"))
     app_logo_36 = ImageSpecField(source='app_logo',
                                       processors=[ResizeToFit(36, 36)],
@@ -28,8 +28,8 @@ class Setting(models.Model):
                                       format='PNG',
                                       options={'quality': 90})
 
-    organization_name = models.CharField(default='WebODM', max_length=255, blank=True, null=True, help_text=_("The name of your organization"), verbose_name=_("Organization name"))
-    organization_website = models.URLField(default='https://github.com/OpenDroneMap/WebODM/', max_length=255, blank=True, null=True, help_text=_("The website URL of your organization"), verbose_name=_("Organization website"))
+    organization_name = models.CharField(default='橙黄橘绿', max_length=255, blank=True, null=True, help_text=_("The name of your organization"), verbose_name=_("Organization name"))
+    organization_website = models.URLField(default='https://space-docs.natsuu.top/', max_length=255, blank=True, null=True, help_text=_("The website URL of your organization"), verbose_name=_("Organization website"))
     theme = models.ForeignKey(Theme, blank=False, null=False, on_delete=models.DO_NOTHING, verbose_name=_("Theme"),
                               help_text=_("Active theme"))
 
